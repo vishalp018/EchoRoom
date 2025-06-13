52bfbf93da69bc386d9b31b8ab83111942951118
VISHAL <palvishal111@gmail.com>
2025-06-13 11:48:22 +0530

    Added the landing Login page

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,10 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./Login.css";
+import assets from "../../assets/assets"
 
 const Login = () => {
+  const [currState,setCurrState]=useState("Sign up");
   return (
-    <div>
-      
+    <div className='login'>
+      <img src={assets.logo_big} alt="" className="logo" />
+      <form  className="login-form">
+        <h2>{currState}</h2>
+        {currState==="Sign up"?<input required type="text" placeholder='Username' className="form-input" />:null}
+        <input required placeholder='Email address' type="email" className="form-input" />
+        <input required type="password" placeholder='password' className="form-input" />
+        <button type='submit'>{currState==="Sign up"?"Create account":"Login now"}</button>
+        <div className="login-term">
+          <input type="checkbox" name="" id="" />
+          <p>Agree to the terms of use & privacy policy.</p>
+        </div>
+        <div className="login-forgot">
+          {
+            currState==="Sign up"?
+            <p className="login-toggle">Already have an account <span onClick={()=>{
+              setCurrState("Login")
+            }}>Login here</span> </p> :
+          <p className="login-toggle">Create an account <span onClick={()=>{
+            setCurrState("Sign up")
+          }}>click here</span> </p>
+          }
+        </div>
+      </form>
     </div>
   )
 }
